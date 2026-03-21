9584e224eef9a4ac6119da086146395a910f6367
scottcarey006 <scottcarey006@gmail.com>
2026-03-21 07:53:34 +0000

    Add files via upload
    Initial commit - JavaScript basics activities

---
+++ basics.js
@@ -0,0 +1,26 @@
+// Activity 1 - Vanilla JS Basics
+
+// a. -5 + 8 * 6
+console.log(-5 + 8 * 6);
+
+// b. (55+9) % 9
+console.log((55 + 9) % 9);
+
+// c. 20 + -3*5 / 8
+console.log(20 + -3 * 5 / 8);
+
+// d. 5 + 15 / 3 * 2 - 8 % 3
+console.log(5 + 15 / 3 * 2 - 8 % 3);
+
+
+// Activity 2 - Divide two numbers
+
+// Ask the user for two numbers
+let num1 = Number(prompt("Input the first number:"));
+let num2 = Number(prompt("Input the second number:"));
+
+// Perform division
+let result = num1 / num2;
+
+// Print the result
+console.log("The division of the first number and the second number is: " + result);
