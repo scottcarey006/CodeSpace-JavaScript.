732fd9c56ac17795eb9b8a6b86150745b828b1af
scottcarey006 <scottcarey006@gmail.com>
2026-03-30 16:21:00 +0100

    Add files via upload

---
+++ javascript Activity 1/script1.js
@@ -0,0 +1,9 @@
+// the function runs when the button is clicked //
+function js_style() {
+// get the text element by the ID ///
+const textElement = document.getElementById("text");
+/////// change style ////////
+    textElement.style.color = "green";
+    textElement.style.fontSize = "40px";
+    textElement.style.fontWeight = "bold";
+}
